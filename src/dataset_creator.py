0460d42a7e846df5fdf9b6c95643878770e1f6b9
javibg96 <blascogarcia.javier@outlook.com>
2020-05-21 18:02:16 +0200

    completamente operativo y con funcionalidades de ingreso de valores por terminal

--- src/dataset_creator.py
+++ src/dataset_creator.py
@@ -7,21 +7,24 @@ import pandas as pd
 import logging
 
 
-def traductor_csv(ruta=None, idioma='es', columnas=None, inicio=0, fin=100):
+def traductor_csv(ruta, nombre_fin, idioma, columnas, inicio, fin):
 
-    if columnas is None:
-        columnas = ['label', 'parent_comment']
 
     google_api = True
-    df = pd.read_csv(ruta)
+    try:
+        df = pd.read_csv(ruta)
+    except:
+        logging.error("fallo leyendo el archivo")
+        raise
 
-    df_temp = df[inicio:fin]  # en un test inicial ya he traducido las primeras 302
+    if columnas is None:
+        columnas = [df[1], df[2]]
+
+    df_temp = df[inicio:fin]  # en un test inicial ya he traducido las primeras 6002
     # print(df_temp.head())
-
     df_clean = df_temp[columnas]
     df_trans = df_clean
-
-    traductor = src.googletrans_scrap.google_trans()
+    traductor = None
 
     try:
         sentence = "testing google api"
@@ -30,6 +33,7 @@ def traductor_csv(ruta=None, idioma='es', columnas=None, inicio=0, fin=100):
         print(frase + ": api OK, continuamos")
     except:
         print("todo correcto, google api no funciona asi que trabajamos con Selenium")
+        traductor = src.googletrans_scrap.google_trans()
         # ponerle un tiempo estimado seria de pro
         google_api = False
 
@@ -40,33 +44,36 @@ def traductor_csv(ruta=None, idioma='es', columnas=None, inicio=0, fin=100):
             if index - inicio < 2999 and google_api:
                 google_translator = Translator()
                 frase = google_translator.translate(sentence, dest=idioma)
-                print(frase)
                 df_trans = df_trans.replace(sentence, frase)
                 # if frase == "":
                 # añadir lo de exceso de caracteres y tal
             else:
-                df_trans = sele_translation(traductor, sentence, index, df_trans)
+                df_trans = sele_translation(traductor, sentence, index, inicio, df_trans, ruta+nombre_fin)
         except:
-            df_trans = sele_translation(traductor, sentence, index, df_trans)
+            google_api = False
+            df_trans = sele_translation(traductor, sentence, index, inicio, df_trans, ruta+nombre_fin)
 
+    print("\n\n----PROCESO FINALIZADO-------")
+    df_trans.to_csv(ruta_fin, sep='|', index=False, header=True)
     traductor.exit_browser()
     # print(df_trans.head())
     print(f"ACUERDATE DE QUE EMPIEZAS EN {inicio}")
 
 
-def sele_translation(traductor, sentence, index, df_trans):
+def sele_translation(traductor, sentence, index, inicio,df_trans, ruta_fin):
     frase = traductor.translate_into_esp(sentence)
     df_trans = df_trans.replace(sentence, frase)
     if not frase:
         print(f"\n\nCHEEEEEEEEEECK {index}  !!!!!!!!!!!!!!!!!!!!!!!!\n\n")
-    if index % 500 == 0:
+    if index % 500 == 0 and index != inicio:
         print(f"\nYa he traducido {index}\n")
-        df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-temp.csv", sep='|',
+
+        df_trans.to_csv(ruta_fin, sep='|',
                         index=False, header=True)
-    if index % 1000 == 0:
+    if index % 1000 == 0 and index != inicio:
         traductor.exit_browser()
-        traductor = googletrans_scrap.google_trans()
-        df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-temp.csv", sep='|',
+        traductor = src.googletrans_scrap.google_trans()
+        df_trans.to_csv(ruta_fin, sep='|',
                         index=False,
                         header=True)
     return df_trans
--- traducir_csv.py
+++ traducir_csv.py
@@ -5,32 +5,86 @@ Created on Tue May 19 16:53:51 2020
 @author: JBLASCO
 """
 import logging
+import os
 from src.dataset_creator import traductor_csv
 
 try:
-    # ruta = input("Introduce la ruta del fichero .csv que quieres traducir")
-    ruta = "../entreno_sarcasmo/train-balanced-sarcasm.csv"
+    FORMAT = '%(asctime)s--%(levelname)s--%(message)s'
+    logging.basicConfig(filename='logs_traductor.log', filemode='a', format=FORMAT, level='INFO')
+    terminalMethod = True  # Si quieres añadir los valores por pantalla terminalMethod = True
+    if terminalMethod:
+        ruta = input("Introduce la ruta del fichero .csv que quieres traducir:\n")
+        # ruta = "../entreno_sarcasmo/"
 
-    idioma = input("introduce el idioma al que quieres traducir:\n").lower()
-    if idioma == "ingles" or idioma == "inglés":
-        lang = "en"
+        if ".csv" not in ruta:
+            nombre_ini = input("Introduce el nombre del fichero .csv que quieres traducir")
+            # nombre_ini = "train-balanced-sarcasm.csv"
+            if ".csv" not in nombre_ini:
+                nombre_ini = nombre_ini + ".csv"
+                ruta = ruta + nombre_ini
+        else:
+            ruta_split = ruta.split("/")
+            nombre_ini = ruta_split[-1]
+            print(f"\nNombre del archivo: {nombre_ini}\n")
+
+        nombre_fin = input("Introduce como quieres llamar al fichero traducido:\n")
+        # nombre_fin = "train-balanced-sarcasm-temp.csv"
+        if ".csv" not in nombre_fin:
+            nombre_fin = nombre_fin + ".csv"
+
+        if nombre_fin == nombre_ini:
+            nombre_fin = nombre_fin.replace(".csv", "(1).csv")
+
+        idioma = input("\nIntroduce el idioma al que quieres traducir:\n").lower()
+        if idioma == "ingles" or idioma == "inglés":
+            lang = "en"
+            # deberias añadir un .json con los idiomas y las posibles abreviaturas, esto es cutre
+        else:
+            lang = idioma[0:2]
+        n_col = 0
+        notInt = True
+        while notInt:
+            try:
+                n_col = int(input("Introduce el número de columnas con las que quieres quedarte del csv:\n"))
+                notInt = False
+            except ValueError:
+                print("Introduce un numero válido")
+                continue
+
+        columnas = list([])
+        for i in range(0, n_col):
+            col = input(f"Introduce el nombre de la columna {i+1}:\n")
+            columnas.append(col)
+        # col_trans = input("¿Cual es la columna a traducir?: \n")
+        # poner un señalizador
+        print(f"columnas elegidas: {columnas}, columna a traducir: {columnas[-1]}")
+
+        notInt = True
+        inicio, fin = 0, 0
+        while notInt:
+            try:
+                inicio = int(input("\nIntroduce desde que fila quieres empezar a traducir: \n"))
+                fin = int(input("\nIntroduce hasta que fila traducir: \n"))
+                notInt = False
+            except ValueError:
+                print("Introduce un numero válido")
+        print(f"\nInicio: {inicio}, Fin: {fin}\n")
+
+        checkNoche = input("\nQuieres que me apague al acabar?: \n")
+        if checkNoche.lower() == "si" or checkNoche.lower() == "yes":
+            os.system("shutdown /s /t 1")   # como tarda mucho, para que se apague solo si lo dejas corriendo x la noche
+        else:
+            print("\n..Empezamos..")
+
     else:
-        lang = idioma[0:2]
+        ruta = "../entreno_sarcasmo/train-balanced-sarcasm.csv"
+        nombre_fin = "train-balanced-sarcasm-temp.csv"
+        lang = "es"
+        columnas = ['label', 'parent_comment']
+        inicio, fin = 7000, 8000
 
-    # n_col = input("introduce el numero de columnas con las que quieres quedarte del csv:\n")
-    # for i in range (0, n_col):
-        # col = input(f"introduce el nombre de la columna {1}:\n")
-        # columnas.append(col)
-    # col_trans = input("¿Cual es la columna a traducir?: \n")
-    # poner un señalizador
+    traductor_csv(ruta, nombre_fin, lang, columnas, inicio, fin)
 
-    columnas = ['label', 'parent_comment']
-
-    # inicio = input("\nIntroduce desde que fila quieres empezar a traducir: \n")
-    # fin = input("\nIntroduce hasta que fila traducir: \n")
-    inicio, fin = 6002, 8000
-
-    traductor_csv(ruta, lang, columnas, inicio, fin)
 
 except:
     logging.exception("Error traceback")
