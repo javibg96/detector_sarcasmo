705a0a587a8ee1755caad5b00e27243801c5c26d
javibg96 <blascogarcia.javier@outlook.com>
2020-05-21 13:04:06 +0200

    mejoras de input y de control de csv

--- dataset_creator.py
+++ dataset_creator.py
@@ -2,44 +2,62 @@
 # utilizo selenium para traducir desde google translator
 
 import googletrans_scrap
+from googletrans import Translator
 import pandas as pd
 import logging
 
-df = pd.read_csv("../entreno_sarcasmo/train-balanced-sarcasm.csv")
-# sentence = "you are so intense"
+# ruta = input("Introduce la ruta del fichero .csv que quieres traducir")
+idioma = input("introduce el idioma al que quieres traducir:\n").lower()
+if idioma == "ingles" or idioma == "inglés":
+    lang = "en"
+else:
+    lang = idioma[0:2]
+ruta = "../entreno_sarcasmo/train-balanced-sarcasm.csv"
+df = pd.read_csv(ruta)
 # lista = [600, 1000, 2000, 3000, 4000, 5000,9000, 12000, 15000, 18000, 21000, 24000, 27000, 30000, 33000, 36000, 39000]
-lista = list([])
-for i in range(9, 20):
-    lista.append(i*500)
-print(lista)
-inicio, fin = 2001, 10001
+# lista = list([])
+# for i in range(7, 20):
+#    lista.append(i*500)
+
+# inicio = input("\nIntroduce desde que fila quieres empezar a traducir: \n")
+# fin = input("\nIntroduce hasta que fila traducir: \n")
+inicio, fin = 5001, 10001
+
 df_temp = df[inicio:fin]  # en un test inicial ya he traducido las primeras 302
-print(df_temp.head())
+# print(df_temp.head())
+
 df_clean = df_temp[['label', 'parent_comment']]
 df_trans = df_clean
+
 tradutor = googletrans_scrap.google_trans()
 
 for index, row in df_clean.iterrows():
-    if index-inicio < fin:
-        sentence = df_clean.iloc[index-inicio]['parent_comment']
+    if index-inicio < fin-inicio:
+        sentence = df_clean.iloc[index - inicio]['parent_comment']
         try:
             # print(f"frase eng: {sentence}")
-            frase = tradutor.translate_into_esp(sentence)
-            df_trans = df_trans.replace(sentence, frase)
+            if index - inicio < 2999:
+                google_translator = Translator()
+                frase = google_translator.translate(sentence, dest="es")
+                df_trans = df_trans.replace(sentence, frase)
+            else:
+                frase = tradutor.translate_into_esp(sentence)
+                df_trans = df_trans.replace(sentence, frase)
             if not frase:
                 print(f"\n\nCHEEEEEEEEEECK {index}  !!!!!!!!!!!!!!!!!!!!!!!!\n\n")
-            if index % 100 == 0:
+            if index % 500 == 0:
                 print(f"\nYa he traducido {index}\n")
-            if index in lista:
-                df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-{index}.csv", sep='|', index=False, header=True)
+                df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-temp.csv", sep='|',
+                                index=False, header=True)
+            if index % 1000 == 0:
                 tradutor.exit_browser()
                 tradutor = googletrans_scrap.google_trans()
-                print(f"Ya he traducido {index}")
 
         except:
             logging.exception("Error traceback")
+            print("\nERROR EN LA API DE GOOGLE\n")
             print(f"\n\nindex con errores:{index}")
-            df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-{index}.csv", sep='|', index=False,
+            df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-temp.csv", sep='|', index=False,
                             header=True)
 tradutor.exit_browser()
 # print(df_trans.head())
--- datatrans_googapi.py
+++ datatrans_googapi.py
@@ -5,10 +5,19 @@ import logging
 import json
 import time
 
+
+# ruta = input("Introduce la ruta del fichero .csv que quieres traducir")
+idioma = input("introduce el idioma al que quieres traducir:\n").lower()
+if idioma == "ingles" or idioma == "inglés":
+    lang = "en"
+else:
+    lang = idioma[0:2]
+
 # GoogleApi
 google_translator = Translator()
 traductor = translate.Translator(to_lang="es")
-sentence = "you are such an asshole"
+
+# sentence = "you are such an asshole"
 
 # GoogleApi, si la usas mucho te banean la VPN
 # frase = google_translator.translate(sentence, dest='es').text
@@ -18,18 +27,26 @@ sentence = "you are such an asshole"
 
 
 df = pd.read_csv("../entreno_sarcasmo/train-balanced-sarcasm.csv")
-df_temp = df.head(500)
+
+# La API de google solo te permite 3000!!
+inicio = 6001
+fin = inicio + 2999
+
+df_temp = df[inicio:fin]
 df_clean = df_temp[['label', 'parent_comment']]
 df_trans = df_clean
+lista = list([])
+for i in range(12, 20):
+    lista.append(i*500)
 list_index_to_check = []
 
 
 for index, row in df_clean.iterrows():
-    try:
-        # GoogleApi, reinicio por errores de VPN
-        google_translator = Translator()
-
-        if index <= 1000:
+    if index - inicio < fin:
+        sentence = df_clean.iloc[index - inicio]['parent_comment']
+        try:
+            # GoogleApi, reinicio por errores de VPN
+            google_translator = Translator()
             sentence = df_clean.iloc[index]['parent_comment']
             # print(f"frase eng: {sentence}")
 
@@ -37,15 +54,21 @@ for index, row in df_clean.iterrows():
 
             df_trans = df_trans.replace(sentence, frase)
 
-    except json.decoder.JSONDecodeError:
+            if index % 500 == 0:
+                print(f"\nYa he traducido {index}\n")
+            if index in lista:
+                df_trans.to_csv(f"../entreno_sarcasmo/entrenamiento-equilibrado-sarcasmo-{index}.csv", sep='|', index=False, header=True)
+                print(f"Ya he traducido {index}")
 
-        print(sentence)
-        frase = traductor.translate(sentence)
-        print("\nERROR EN LA API DE GOOGLE, TRADUCIDO POR LA API DE TRADUCCION")
-        df_trans = df_trans.replace(sentence, frase)
-        list_index_to_check.append(index)
-        logging.exception("error traceback")
-        continue
+        except json.decoder.JSONDecodeError:
+
+            print(sentence)
+            frase = traductor.translate(sentence)
+            print("\nERROR EN LA API DE GOOGLE, TRADUCIDO POR LA API DE TRADUCCION")
+            df_trans = df_trans.replace(sentence, frase)
+            list_index_to_check.append(index)
+            logging.exception("error traceback")
+            raise
 
 print(f"LIST: {list_index_to_check}")
 text_file = open("list_to_check.txt", "w")
